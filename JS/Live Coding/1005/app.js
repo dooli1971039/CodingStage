e7827e94370ce5e59930e37849d0b003f2d27ccf
dooli1971039 <dooli9819@gmail.com>
2022-10-03 05:31:44 +0900

    live coding 준비

---
+++ JS/Live Coding/1005/app.js
@@ -0,0 +1,26 @@
+let a=3;
+let b=1.88;
+
+console.log(a);
+console.log(b);
+
+console.log(typeof a);
+console.log(typeof b);
+
+let c=Infinity;
+let d=-Infinity;
+let e= NaN;
+
+console.log(c);
+console.log(d);
+console.log(e);
+
+let str1="안녕";
+let str2='하세요';
+console.log(str1 + str2);
+
+let t=true;
+let ss="true";
+
+console.log(typeof t);
+console.log(typeof ss);
